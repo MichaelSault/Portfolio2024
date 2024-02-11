720bfa4da798373ca19c35402c1351683ee8f17f
MichaelSault <michael_sault@hotmail.com>
2024-02-10 23:50:13 -0500

    set up landing component/row

---
+++ src/components/Landing.component.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Landing() {
+
+    return (
+        <>
+            <div className='landingRow'>
+                <h1>Michael Sault</h1>
+            </div>
+        </>
+    )
+}
+
+export default Landing;
