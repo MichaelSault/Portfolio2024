180c6a263fce6f838d7c2cdaf4ec6714277c7b96
MichaelSault <michael_sault@hotmail.com>
2024-02-12 23:42:35 -0500

    started working on a carousel for my projects

---
+++ src/components/Carousel.component.jsx
@@ -0,0 +1,18 @@
+import '../App.css';
+import '../styleSheets/carousel.css';
+
+function Carousel() {
+
+    return (
+        <div
+            style={{
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center center",
+            }}
+            >
+        </div>
+    )
+}
+
+export default Carousel;
--- src/components/Portfolio.component.jsx
+++ src/components/Portfolio.component.jsx
@@ -1,11 +1,14 @@
 import '../App.css';
 
+import Carousel from './Carousel.component.jsx';
+
 function Portfolio() {
 
     return (
         <>
             <div className='portfolioRow'>
                 <h1 className='portfolioTitle'>Coming Soon</h1>
+                <Carousel />
             </div>
         </>
     )
