503eed906315f5e43837399a77633de5d876f6a5
MichaelSault <michael_sault@hotmail.com>
2024-02-23 00:00:00 -0500

    worked on logic for carousel

--- src/components/CarouselItem.component.jsx
+++ src/components/CarouselItem.component.jsx
@@ -7,11 +7,21 @@ function CarouselItem({item, active}){
     console.log(item.image.default);
     console.log(item.title, active);
     return (
-        <div className="carousel-item" display={`${active? "block":"hidden"}`}>
-            <div></div>
-            <img className="carousel-image" src={item.image}/>
-            <div className="carousel-item-description">{item.description}</div>
+        <div>
+            {active = true
+                ? <div className="carousel-item">
+                    <div></div>
+                    <img className="carousel-image" src={item.image}/>
+                    <div className="carousel-item-description">{item.description}</div>
+                    <div>{active}</div>
+                </div>
+                : <div>this should be hidden</div>
+            }
+            
         </div>
+            
+        
+        
     )
 }
 
