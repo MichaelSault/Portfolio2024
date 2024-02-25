41d0e271355af3c648eefb8268cee0335ba269b5
MichaelSault <michael_sault@hotmail.com>
2024-02-24 23:56:04 -0500

    front-end changes to project carousel

--- src/components/CarouselItem.component.jsx
+++ src/components/CarouselItem.component.jsx
@@ -9,7 +9,7 @@ function CarouselItem({item}){
             <div className="carousel-item">
                 <div></div>
                 <img className="carousel-image" src={item.image}/>
-                <div className="carousel-item-description">{item.description}</div>
+                <div className="carousel-item-description"><h2>{item.title}</h2>{item.description}</div>
             </div>            
         </div>
             
